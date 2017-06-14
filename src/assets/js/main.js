8b84aa7a4f6602e0344eafce551dc48c5692ad09
Martijn Hermans <martijn@touchtribe.nl>
2017-06-14 14:03:57 +0200

    booh

--- src/assets/js/main.js
+++
@@ -1,8 +0,0 @@
-import { Bear } from './bear';
-import { Koala } from './koala';
-
-const bear = new Bear();
-const koala = new Koala({ name: 'Tom' });
-
-console.log(bear.sayHello());
-console.log(koala.sayHello());
