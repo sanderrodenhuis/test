6a56249b1ebb8522962c4be8e865b20c2a610a17
Olga Karaseva <olga@touchtribe.nl>
2017-06-27 16:30:59 +0200

    add icons half working filtering fn

--- server/controllers/funnel/select-task.js
+++ server/controllers/funnel/select-task.js
@@ -1,3 +1,12 @@
-module.exports = function(req, res, next) {
-  res.render('pages/funnel/select-task');
+const Api = require('../../utils/api');
+
+module.exports = async function(req, res, next) {
+  try {
+    const categories = await Api.fetchJobCategories();
+    res.render('pages/funnel/select-task', {
+      categories
+    });
+
+  } catch(e ) { console.log(e); }
+
 };
