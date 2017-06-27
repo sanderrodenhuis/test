7fdd93352649230cd1170ba8bd10fb4c1b7c3c90
Olga Karaseva <olga@touchtribe.nl>
2017-06-27 16:30:59 +0200

    add icons half working filtering fn

--- client/src/assets/js/main.js
+++ client/src/assets/js/main.js
@@ -2,7 +2,7 @@ import $ from 'jquery'
 import './modal';
 
 
-
+window.$ = $;
 
 $('.site-nav__item--toggle').on('click', function (e) {
   e.preventDefault();
@@ -12,3 +12,58 @@ $('.site-nav__item--toggle').on('click', function (e) {
 $('.site-menu__btn-close').on('click', function () {
   $('.site-menu--mobile').removeClass('is-visible')
 })
+
+
+
+
+$('body').on('click', '.filter-list__btn', (e) => {
+  e.preventDefault();
+  let id = e.target.getAttribute('data-category-id');
+
+  $('.search-results').trigger('filter.jobs', [id]);
+});
+
+$('.form__control--funnel-search').bind('input', function (e) {
+  console.log($(e.target).val());
+  const query = $(e.target).val();
+  $('.search-results').trigger('filter.jobs',[undefined, query]);
+
+});
+
+
+
+$('body').on('filter.jobs','.search-results',(event, categoryId = '*', query = '') => {
+  console.log(event, categoryId, query);
+
+  const $searchResults = $(event.target),
+    $resultList = $searchResults.find('.result-list'),
+    $results = $resultList.children();
+
+  $results.show();
+
+  if (categoryId != '*')
+  {
+    $results.filter((idx, elem) => elem.getAttribute('data-category-id') !== categoryId).hide();
+  }
+  if (query)
+  {
+    $results.filter((ids, elem) => {
+      const matchPattern = new RegExp(query, 'i');
+      return !$(elem).children().text().match(matchPattern)
+
+    }).hide()
+  }
+
+});
+
+
+
+
+// function showResults(id) {
+//   console.log(id);
+//
+//   $('.result-list').children().filter(function(idx, el) {
+//       console.log(el, idx);
+//       el.getAttribute('data-category-id') !== categoryId).hide();
+//   })
+// }
