173dd7c61c213e395d38db90a33449efd65ae0d7
Martijn Hermans <martijn@touchtribe.nl>
2017-06-28 11:21:13 +0200

    Implementing Api.login

--- server/utils/api.js
+++ server/utils/api.js
@@ -1,11 +1,17 @@
-var Mendix = require('./mendix');
+const Mendix = require('./mendix');
 
-let Api = {}
+let Api = {};
 
 Api.fetchJobCategories = async () => {
   return Mendix.get('/jobcategories/')
     .then(urls => Promise.all(urls.data.map(url => Mendix.get(url))))
     .then(categories => [].concat(...categories.map(category => category.data)));
+};
+
+
+Api.login = async (username, password) => {
+  let {data} = await Mendix.forUser(username,password).get('/account');
+  return data;
 };
 
 module.exports = Api;
