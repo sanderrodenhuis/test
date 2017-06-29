193b6d31b6bcce671f34a4b30ff4f371fed512b7
Martijn Hermans <martijn@touchtribe.nl>
2017-06-29 15:52:24 +0200

    intermediary commit to prepare for demo

--- server/utils/api.js
+++ server/utils/api.js
@@ -8,6 +8,18 @@ Api.fetchJobCategories = async () => {
     .then(categories => [].concat(...categories.map(category => category.data)));
 };
 
+Api.fetchOrders = async () => {
+  return Mendix.get('/orders')
+    .then(urls => Promise.all(urls.data.map(url => Mendix.get(url))))
+    .then(orders => [].concat(...orders.map(order => order.data)));
+};
+Api.fetchOrder = async (orderId) => {
+  return Mendix.get('/orders/'+orderId)
+    .then(order => order.data);
+};
+Api.cancelOrder = async (orderId) => {
+
+};
 
 Api.login = async (username, password) => {
   let {data} = await Mendix.forUser(username,password).get('/account');
