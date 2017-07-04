2b2634b6ac66ce0a97cd3076281c882008f3895e
Martijn Hermans <martijn@touchtribe.nl>
2017-07-04 15:43:48 +0200

    i messed up...

--- client/src/assets/js/datepicker.js
+++ client/src/assets/js/datepicker.js
@@ -2,49 +2,50 @@ import $ from './jquery'
 import 'jquery-ui'
 import 'jquery-datepicker'
 
-
-/*
- todo: clean up later
- */
-const $datepicker = $( "#datepicker" );
-$datepicker.datepicker({
-  closeText: "Sluiten",
-  prevText: "",
-  nextText: "",
-  currentText: "Vandaag",
-  monthNames: [ "Januari", "Februari", "Maart", "April", "Mei", "Juni",
-    "Juli", "Augustus", "September", "Oktober", "November", "December" ],
-  monthNamesShort: [ "jan", "feb", "mrt", "apr", "mei", "jun",
-    "jul", "aug", "sep", "okt", "nov", "dec" ],
-  dayNames: [ "zondag", "maandag", "dinsdag", "woensdag", "donderdag", "vrijdag", "zaterdag" ],
-  dayNamesShort: [ "zon", "maa", "din", "woe", "don", "vri", "zat" ],
-  dayNamesMin: [ "Z", "M", "D", "W", "D", "V", "Z" ],
-  weekHeader: "Wk",
-  dateFormat: "DD dd MM yy",
-  firstDay: 0,
-  minDate: 0,
-  isRTL: false,
-  showOtherMonths: true,
-  showMonthAfterYear: false,
-  yearSuffix: ""
+$(() => {
+  const $body = $('body');
+  
+  console.log('testing');
+  const $datepicker = $( "#datepicker" );
+  $datepicker.datepicker({
+    closeText: "Sluiten",
+    prevText: "",
+    nextText: "",
+    currentText: "Vandaag",
+    monthNames: [ "Januari", "Februari", "Maart", "April", "Mei", "Juni",
+      "Juli", "Augustus", "September", "Oktober", "November", "December" ],
+    monthNamesShort: [ "jan", "feb", "mrt", "apr", "mei", "jun",
+      "jul", "aug", "sep", "okt", "nov", "dec" ],
+    dayNames: [ "zondag", "maandag", "dinsdag", "woensdag", "donderdag", "vrijdag", "zaterdag" ],
+    dayNamesShort: [ "zon", "maa", "din", "woe", "don", "vri", "zat" ],
+    dayNamesMin: [ "Z", "M", "D", "W", "D", "V", "Z" ],
+    weekHeader: "Wk",
+    dateFormat: "DD dd MM yy",
+    firstDay: 0,
+    minDate: 0,
+    isRTL: false,
+    showOtherMonths: true,
+    showMonthAfterYear: false,
+    yearSuffix: ""
+  });
+  
+  
+  $datepicker.on("change",function(){
+    $('.datepicker-info__date').html($(this).val());
+  });
+  $body.on('click','.time-list__link',(event) => {
+    event.preventDefault();
+    const $target = $(event.target),
+          time = $target.text(),
+          $list = $target.closest('.time-list');
+          
+    if (! $target.hasClass('is-active'))
+      return;
+    
+    $('.datepicker-info__time').html(time);
+    
+    $list.find('.is-selected').removeClass('is-selected');
+    $target.addClass('is-selected');
+  });
+  
 });
-
-
-$datepicker.on("change",function(){
-  const selectedDate = $(this).val();
-  const $datepickerContainer = $(this).closest('.datepicker__container');
-
-  $datepickerContainer.find('.datepicker-info__date').html(selectedDate);
-
-
-});
-$('body').on('click','.time-list__link',(event) => {
-  event.preventDefault();
-  const $target = $(event.target),
-    $datepickerContainer = $target.closest('.datepicker__container'),
-    $targetValue = $(event.target).text();
-  if($target.hasClass('is-active')) {
-    $datepickerContainer.find('.datepicker-info__time').html($targetValue);
-  }
-});
-
