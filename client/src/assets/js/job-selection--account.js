6046aa492e2f78412f1e65c50a2e6c249a92e724
Olga Karaseva <olga@touchtribe.nl>
2017-07-18 12:48:16 +0200

    close dropdown when you click on an item small changes

--- client/src/assets/js/job-selection--account.js
+++ client/src/assets/js/job-selection--account.js
@@ -1,22 +1,7 @@
 import $ from 'jquery'
 
 $(() => {
-  const $body = $('body');
-  // expand and collapse for the funnel/task-selection/filter-list
-  $body.on('click','.filter-list__title',(event) => {
-    event.preventDefault();
-    const $target = $(event.target),
-      $filterList = $target.closest('.filter-list');
-    $filterList.toggleClass('is-expanded');
-  });
-  $body.on('click','.filter-list__link',(event) => {
-    event.preventDefault();
-    const $target = $(event.target),
-      $filterList = $target.closest('.filter-list');
-    $filterList.find('.is-active').removeClass('is-active');
-    $target.addClass('is-active');
-    $filterList.trigger('filtered');
-  });
+
 
   $('.selection').each((idx, elem) => {
     const $selection = $(elem),
--- client/src/assets/js/job-selection.js
+++ client/src/assets/js/job-selection.js
@@ -13,18 +13,19 @@ $(() => {
     event.preventDefault();
     const $target = $(event.target),
           $filterList = $target.closest('.filter-list');
-    
+
     $filterList.find('.is-active').removeClass('is-active');
     $target.addClass('is-active');
+    $filterList.toggleClass('is-expanded');
     $filterList.trigger('filtered');
   });
-  
+
   $('.job-selection').each((idx, elem) => {
     const $jobSelection = $(elem),
           $jobSelectionForm = $jobSelection.find('.job-selection__form'),
           $jobSelectionFilters = $jobSelection.find('.job-selection__filters'),
           $jobSelectionJobs = $jobSelection.find('.job-selection__jobs');
-    
+
     const fnFilter = () => {
       const query = $jobSelectionForm.find('input').val(),
             categoryId = $jobSelectionFilters.find('.is-active').data('category-id');
@@ -32,15 +33,15 @@ $(() => {
     };
     $jobSelectionForm.on('keyup','input',fnFilter);
     $jobSelectionFilters.on('filtered',fnFilter);
-    
+
     $jobSelectionJobs.on('filter',(event, query = '', categoryId = '*') => {
       const $items = $jobSelectionJobs.find('.job-list__item');
       categoryId = categoryId.toString();
       $items.removeClass('is-hidden');
-      
+
       if (categoryId !== '*')
         $items.filter((idx,elem) => $(elem).data('category-id').toString() !== categoryId).addClass('is-hidden');
-      
+
       if (query)
       {
         const regex = new RegExp(query, 'i');
