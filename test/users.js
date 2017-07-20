072baf62b06ea02e79ed4903c20e7533a2e0f80d
giuseppe <giuseppe@techtribe.nl>
2017-07-20 18:53:19 +0200

    added test for POST user/emailcheck GET users/id

--- test/users.js
+++ test/users.js
@@ -8,10 +8,18 @@ let chai = require('chai');
 var chaiAsPromised = require("chai-as-promised");
 
 // Then either:
-let {expect, assert} = chai;
+let {expect }= chai;
+// let {expect, assert} = chai;
+
+
+chai.use(chaiAsPromised);
+
 let test_user = process.env.TEST_USER,
-  test_pass = process.env.TEST_PASS;
-let login_res=['HouseNumber',
+  test_pass = process.env.TEST_PASS,
+  admin_user = process.env.MENDIX_USER,
+  admin_pass = process.env.MENDIX_PASS;
+
+let user_res=['HouseNumber',
   'Addition',
   'Email',
   'IBAN',
@@ -25,7 +33,7 @@ let login_res=['HouseNumber',
   'LastName',
   'PostCode',
   'IdUser']
-chai.use(chaiAsPromised);
+
 //Our parent block
 describe('Users', () => {
   beforeEach((done) => {
@@ -36,7 +44,6 @@ describe('Users', () => {
   * Test the /GET route
   */
 
-
   describe('/GET user/login', () => {
     it('it should LOGIN auth as test user', async () => {
       const response = await mendix.forUser(test_user, test_pass).get('/user/login'),
@@ -46,7 +53,27 @@ describe('Users', () => {
       expect(data)
         .to.be.an('object')
         .and
-        .to.have.keys(login_res)
+        .to.have.keys(user_res)
+
+      describe('POST /user/emailcheck', () => {
+        it(`Should check if the email exists`, async () => {
+          const response = await mendix.forUser(admin_user, admin_pass).post('/user/emailcheck',{"email":data.Email});
+          expect (response).to.have.property('status',200);
+
+        })
+      })
+
+      describe('GET /users/{id}', () => {
+        it(`Should get user info`, async () => {
+          const response = await mendix.get('/users/'+data.IdUser),
+            resUser = response.data;
+          expect (response).to.have.property('status',200);
+          expect (resUser).to.be.an('object');
+          expect (resUser).to.have.keys(user_res)
+          expect(resUser).to.deep.equal(data);
+
+        })
+      })
 
     });
   });
