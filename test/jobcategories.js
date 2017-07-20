59aeaef737ad2ebeb92fb621ea2a528682117e62
giuseppe <giuseppe@techtribe.nl>
2017-07-20 17:37:36 +0200

    added job check and job categories check todo wait the promised ready

--- test/jobcategories.js
+++ test/jobcategories.js
@@ -8,10 +8,16 @@ let chai = require('chai');
 var chaiAsPromised = require("chai-as-promised");
 
 // Then either:
-let {expect, assert} = chai;
+let {expect }= chai;
+// let {expect, assert} = chai;
 
 
 chai.use(chaiAsPromised);
+
+let jobcategories_res=[
+  "IdJobCategory",
+  "Jobs",
+  "Name"]
 //Our parent block
 describe('Job Categories', () => {
   beforeEach((done) => {
@@ -23,13 +29,34 @@ describe('Job Categories', () => {
   */
 
 
-  describe('/GET jobcategories', () => {
-    it('it should GET all the jobcategories', () => {
-      return Promise.all([
-        expect(mendix.get('/jobcategories')).to.eventually.have.property('status',200),
-        expect(mendix.get('/jobcategories').then((response)=>{return response.data})).to.eventually.be.an('array')
-      ]);
-    });
+  let jobcategories = [];
+  describe('GET /jobcategories/?data=true', () => {
+    it('Should GET all the jobcategories', async () => {
+      const response = await mendix.get('/jobcategories/?data=true'),
+        jobcategories = response.data;
+      expect (response).to.have.property('status',200);
+      expect (jobcategories).to.be.an('array');
+      jobcategories.map((jobcategory) => {
+        expect (jobcategory).to.have.keys(jobcategories_res)
+        describe('GET /jobcategories/{id}', () => {
+          it(`Should get jobcategory`, async () => {
+            return Promise.all(jobcategories.map(async (jobcategory) => {
+              try {
+                let response = await mendix.get('/jobcategories/'+jobcategory.IdJobCategory),
+                  resJob = response.data;
+                expect (response).to.have.property('status',200);
+                expect (resJob).to.be.an('object');
+                expect (resJob).to.have.keys(jobcategories_res)
+                expect(resJob).to.deep.equal(jobcategory);
+              } catch(response) {
+                assert(response.status !== 200, 'JobID ' + jobcategory.IdJobCategory + ' failed');
+              }
+            }));
+          })
+        })
+      })
+      return Promise.resolve(jobcategories);
+    })
   });
-
 });
+
--- test/jobs.js
+++ test/jobs.js
@@ -37,7 +37,7 @@ describe('Jobs', () => {
   */
 
 
-  let jobs =[{IdJob:2},{IdJob:3}];
+  let jobs = [];
   describe('GET /jobs/?data=true', () => {
     it('Should GET all the jobs', async () => {
       const response = await mendix.get('/jobs/?data=true'),
@@ -46,27 +46,28 @@ describe('Jobs', () => {
       expect (jobs).to.be.an('array');
       jobs.map((job) => {
         expect (job).to.have.keys(jobs_res)
+        describe('GET /jobs/{id}', () => {
+          it(`Should get job`, async () => {
+            return Promise.all(jobs.map(async (job) => {
+              try {
+                let response = await mendix.get('/jobs/'+job.IdJob),
+                  resJob = response.data;
+                expect (response).to.have.property('status',200);
+                expect (resJob).to.be.an('object');
+                expect (resJob).to.have.keys(jobs_res)
+                expect(resJob).to.deep.equal(job);
+              } catch(response) {
+                assert(response.status !== 200, 'JobID ' + job.IdJob + ' failed');
+              }
+            }));
+          })
+        })
       })
-      return;
+      return Promise.resolve(jobs);
     })
   });
 
-  describe('GET /jobs/{id}', () => {
-    it(`Should get job`, async () => {
-      return Promise.all(jobs.map(async (job) => {
-        try {
-          let response = await mendix.get('/jobs/'+job.IdJob),
-            resJob = response.data;
-          expect (response).to.have.property('status',200);
-          expect (resJob).to.be.an('object');
-          expect (resJob).to.have.keys(jobs_res)
-          // expect(resJob).to.deep.equal(job);
-        } catch(response) {
-          // assert(response.status !== 200, 'JobID ' + job.IdJob + ' failed');
-        }
-      }));
-    })
-  })
+
 
 });
 
