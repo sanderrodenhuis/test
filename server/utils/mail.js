ee6f6383166e07994a069b4d70e812401b255eb5
Martijn Hermans <martijn@touchtribe.nl>
2017-07-20 13:31:34 +0200

    Slight update to use environment variables for the sender.

--- server/utils/mail.js
+++ server/utils/mail.js
@@ -2,14 +2,16 @@ const Mailgun = require('mailgun-js');
 const juice = require('juice');
 
 class EmailMiddleware {
-  constructor(app, {apiKey, domain} = {})
+  constructor(app, {apiKey, domain, from} = {})
   {
     this.app = app;
     this.mailgun = Mailgun({
       apiKey: apiKey || process.env.MAILGUN_APIKEY,
       domain: domain || process.env.MAILGUN_DOMAIN
     });
-    
+    this.options = {
+      from: from || process.env.MAILGUN_SENDER
+    };
     app.mail = this;
   }
   async send(to, subject, template, data)
@@ -27,13 +29,16 @@ class EmailMiddleware {
   }
   async sendMail(to, subject, html)
   {
-    return new Promise((resolve, reject) => {
-      this.mailgun.messages().send({
-        from: 'martijn@touchtribe.nl',
+    const options = Object.assign({},
+      this.options,
+      {
         to,
         subject,
         html
-      }, (error, body) => {
+      }
+    );
+    return new Promise((resolve, reject) => {
+      this.mailgun.messages().send(options, (error, body) => {
         error ? reject(error) : resolve(body);
       })
     })
