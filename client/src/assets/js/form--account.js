06b029f131e7bd8b4defc69dc0bedf8b3b694e42
Olga Karaseva <olga@touchtribe.nl>
2017-08-16 11:23:25 +0200

    fix the iban appearance for premium subscription

--- client/src/assets/js/form--account.js
+++ client/src/assets/js/form--account.js
@@ -2,35 +2,35 @@ import $ from './jquery';
 
 $(() => {
   const $body = $('body');
-  
+
   $('.form--account-create, .form--account-edit').each((idx, elem) => {
     const $form = $(elem),
           $blocks = $form.find('.block-subscription'),
           $checkbox = $('#HasSubscription'),
           $formGroup = $checkbox.parent();
-    
+
     $form.on('click', '.block-subscription .btn', (event) => {
       event.preventDefault();
-  
+
       const $block = $(event.target).closest('.block-subscription');
       $blocks.removeClass('is-selected');
       $block.addClass('is-selected');
-  
+
       $checkbox
-        .attr('checked', $blocks.index($block) == 0)
+        .attr('checked', $blocks.index($block) == 1)
         .trigger('change');
     });
-  
+
     $checkbox.on('change', () => {
       $formGroup.toggleClass('is-hidden', $checkbox.is(':checked'));
     }).trigger('change');
-    
+
   }).on('submit', (event) => {
     event.preventDefault();
     const form = event.target,
           $form = $(form),
           href = $form.attr('action');
-    
+
     $.post(location.href, $form.serializeArray())
       .then(response => {
         $form.trigger('errors.reset');
