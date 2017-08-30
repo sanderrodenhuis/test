7dda4c717b4176ae16e6aca89b010bafcb8709c3
Martijn Hermans <martijn@touchtribe.nl>
2017-08-30 14:37:32 +0200

    more fixes

--- server/controllers/account/overview.js
+++ server/controllers/account/overview.js
@@ -13,17 +13,25 @@ router.get('/', HtmlHandler( async (req, res) => {
     res.locals.orders.forEach(order => {
       order.job = res.locals.jobs.find(job => job.IdJob === order.IdJob);
       order.Amount = 1;
-      
       if (order.AdditionalAnswers)
       {
-      
+        order.AdditionalAnswers.forEach(({Answer, IdAdditionalQuestion}) => {
+          let question = order.job.AdditionalQuestions.find(question => question.IdAdditionalQuestion === IdAdditionalQuestion);
+          if (! question || question.AnswerType !== 'Integer')
+            return false;
+          order.Amount = parseFloat(Answer);
+          return true;
+        });
       }
       
     });
-
-    res.locals.orders = res.locals.orders.filter(order => order.job);
+    res.locals.orders = res.locals.orders.filter(order => order.job).sort((a,b) => Date.parse(a.FulfillmentDateTime) - Date.parse(b.FulfillmentDateTime));
+    let pastOrders = res.locals.orders.filter(order => Date.parse(order.FulfillmentDateTime) < Date.now());
+    let futureOrders = res.locals.orders.filter(order => Date.parse(order.FulfillmentDateTime) >= Date.now());
+    res.locals.orders = futureOrders.concat(pastOrders);
     res.render('pages/account/overview');
   } catch(e) {
+    console.log(e);
     throw new ApplicationError('Uw account overzicht kan momenteel niet worden geladen. Probeer het later opnieuw.');
   }
 }));
